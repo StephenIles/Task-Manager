71cdf800172be77016daf2755dde42d8000d611f
StephenIles <stephenciles@gmail.com>
2024-06-15 15:21:30 +0100

    Setup the database schema

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -11,4 +11,8 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
 db = SQLAlchemy(app)
 
+def create_tables():
+    with app.app_context():
+        db.create_all()
+
 from taskmanager import routes
