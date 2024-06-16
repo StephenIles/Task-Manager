75aae55cb102d0feaea680097052174037457ca4
StephenIles <stephenciles@gmail.com>
2024-06-16 15:03:58 +0100

    Final Push

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -12,7 +12,7 @@ class Category(db.Model):
     
     
 class Task(db.Model):
-    # schema for the Task model
+    # schema for the Task models
     id = db.Column(db.Integer, primary_key=True)
     task_name = db.Column(db.String(50), unique=True, nullable=False)
     task_description = db.Column(db.Text, nullable=False)
